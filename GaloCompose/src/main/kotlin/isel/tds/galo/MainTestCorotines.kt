5bd5a9f4a49512493f555fa9bbdd31e1a0baddc4
Afonso Remédios <afonso.remedios@isel.pt>
2024-12-05 20:13:39 +0000

    Corotines in compose tests

---
+++ GaloCompose/src/main/kotlin/isel/tds/galo/MainTestCorotines.kt
@@ -0,0 +1,37 @@
+package isel.tds.galo
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+fun testLog(lab: String) =
+    println("$lab: ${Thread.currentThread().name}")
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        val scope = rememberCoroutineScope()
+        var job by remember { mutableStateOf<Job?>(null) }
+        val clickable = job == null
+
+        Row {
+            testLog("Redraw...")
+            Button(enabled = clickable, onClick = {
+                println("Clicked")
+                job = scope.launch {
+                    repeat(5) { print('.'); delay(1000) }
+                    job = null
+                }
+            } ) { Text("Click me") }
+            Button(enabled = !clickable, onClick = {
+                job?.cancel().also { job = null }
+            } ) { Text("Enable Click") }
+        }
+    }
+}
